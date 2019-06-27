e40386ae9c42b47dbaa2141bd9eccf29471157da
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-27 08:35:31 +0300

    Listener следит какая кнопка была нажата ...

--- app/src/main/java/com/omelchenkoaleks/toolsshop/MainActivity.java
+++ app/src/main/java/com/omelchenkoaleks/toolsshop/MainActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+    private ListView mToolsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,5 +27,14 @@ public class MainActivity extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        mToolsListView = findViewById(R.id.tools_list_view);
+        mToolsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(parent.getContext(),
+                        "Позиция: " + position, Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
