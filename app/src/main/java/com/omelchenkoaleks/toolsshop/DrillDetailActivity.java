9916046f07d71c7994298270f177569afa61849b
omelchenkoaleks <omelchenkoaleks@gmail.com>
2019-06-27 09:49:19 +0300

    получаем переданные данные из Интента и выводим в активности детализации ...

--- app/src/main/java/com/omelchenkoaleks/toolsshop/DrillDetailActivity.java
+++ app/src/main/java/com/omelchenkoaleks/toolsshop/DrillDetailActivity.java
@@ -1,11 +1,17 @@
 package com.omelchenkoaleks.toolsshop;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
+
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
-
 public class DrillDetailActivity extends AppCompatActivity {
+    private ImageView mDrillImageView;
+    private TextView mTitleTextView;
+    private TextView mInfoTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,5 +20,25 @@ public class DrillDetailActivity extends AppCompatActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.hide();
+
+        mDrillImageView = findViewById(R.id.drill_image_view);
+        mTitleTextView = findViewById(R.id.title_text_vew);
+        mInfoTextView = findViewById(R.id.info_text_view);
+
+        Intent intent = getIntent();
+        if (intent.hasExtra("title")
+                && intent.hasExtra("info")
+                && intent.hasExtra("imageId")) {
+            String title = intent.getStringExtra("title");
+            String info = intent.getStringExtra("info");
+            int imageId = intent.getIntExtra("imageId", -1);
+
+            mTitleTextView.setText(title);
+            mInfoTextView.setText(info);
+            mDrillImageView.setImageResource(imageId);
+        } else {
+            Intent backToCategory = new Intent(this, DrillCategoryActivity.class);
+            startActivity(backToCategory);
+        }
     }
 }
